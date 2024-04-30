761753c9f322d2de450b85947dc0a56fb1883979
Kaique Miranda <kaique.miranda1910@gmail.com>
2024-04-30 22:26:39 +0000

    Initial commit

---
+++ 20240312.py
@@ -0,0 +1,18 @@
+
+# 1) Calculo da media de 2 numeros
+
+# n1 = int(input('Digite um número >>  '))
+# n2 = int(input('Digite outro número >>  '))
+# media = (n1 + n2) / 2
+
+# print(f'A media dos numeros {n1} e {n2} é igual a {media}')
+
+#=========================================================
+
+# 2) Taximetro
+
+km_custo = float(input('Digite o valor por km >>  '))
+km_rodado = int(input("Digite a quantidade de km rodados >>  "))
+pagar = km_rodado * km_custo
+
+print(f" Valor KM: R${km_custo} \n rodou {km_rodado}KM \n valor pagar: R${pagar}")
---
+++ main.py
@@ -0,0 +1,13 @@
+# A entrada de dados é feita pelo comando input
+
+nome = str(input("Digite seu nome: "))
+idade = int(input("Digite sua idade: "))
+
+print(f"Seu nome é {nome} e sua idade é {idade}")
+
+idade_futuro = idade + 10
+
+print(f"Sua idade daqui 10 anos será {idade_futuro}")
+
+
+
